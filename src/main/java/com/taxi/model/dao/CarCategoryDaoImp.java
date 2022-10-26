93ce3c216e5e5d3f472031f134e2b7918ad06632
Vlad Bugrak <vlad_bugrak@ukr.net>
2022-10-27 00:11:14 +0300

    dao 1.0.3.6 added CarCategoryDaoImp methods. Haven't checked them yet

--- src/main/java/com/taxi/model/dao/CarCategoryDaoImp.java
+++ src/main/java/com/taxi/model/dao/CarCategoryDaoImp.java
@@ -1,9 +1,12 @@
 package com.taxi.model.dao;
 
 import com.taxi.controller.exceptions.NonUniqGeoPointException;
+import com.taxi.controller.exceptions.ObjectNotFoundException;
 import com.taxi.model.entity.CarCategory;
+import com.taxi.model.entity.GeoPoint;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarCategoryDaoImp implements CarCategoryDao{
@@ -56,28 +59,131 @@ public class CarCategoryDaoImp implements CarCategoryDao{
         }
     }
 
+    private CarCategory extractCarCategoryFromResultSet(ResultSet resultSet) throws SQLException {
+
+        CarCategory carCategory = new CarCategory();
+        carCategory.setId(resultSet.getInt("id"));
+        carCategory.setName(resultSet.getString("name"));
+        carCategory.setNameUA(resultSet.getString("name_ua"));
+
+
+        return carCategory;
+    }
+
     @Override
     public CarCategory findById(int id) {
-        return null;
+        String query = """
+                SELECT * from car_category where id=?;
+                """;
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                CarCategory carCategory = extractCarCategoryFromResultSet(resultSet);
+                return carCategory;
+            } else {
+                return null;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<CarCategory> findAll() {
-        return null;
+        String query = """
+                SELECT * from car_category;
+                """;
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<CarCategory> carCategoryList = new ArrayList<>();
+            CarCategory carCategory;
+            while (resultSet.next()){
+                carCategory = extractCarCategoryFromResultSet(resultSet);
+                carCategoryList.add(carCategory);
+            }
+            return carCategoryList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public boolean update(CarCategory entity) {
-        return false;
+    public boolean update(CarCategory carCategory) {
+
+        if(findById(carCategory.getId()) == null){
+
+            StringBuffer stringBuffer = new StringBuffer();
+            stringBuffer
+                    .append("The CarCategory object with id =")
+                    .append(carCategory.getId())
+                    .append(" does not exist in the data base. So you can't update it");
+            throw  new ObjectNotFoundException(stringBuffer.toString());
+        }
+
+        String query = """
+                UPDATE car_category 
+                SET 
+                name=?,
+                name_ua=?,                   
+                where id=?
+                """;
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+
+
+
+
+            preparedStatement.setString(1,carCategory.getName());
+            preparedStatement.setString(2,carCategory.getNameUA());
+
+
+            return preparedStatement.executeUpdate()>0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+
+        String query = """           
+               DELETE from car_category 
+               where id=?;
+                """;
+
+
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+
+            preparedStatement.setInt(1, id);
+            return preparedStatement.executeUpdate()>0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void close() {
-
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
