12e9d7cd2b23c92f6b6f255c1e8a0332ae18347f
Vlad Bugrak <vlad_bugrak@ukr.net>
2022-11-07 03:50:37 +0200

    dao 1.0.5.1 finished the Discount class.

--- src/main/java/com/taxi/model/entity/Discount.java
+++ src/main/java/com/taxi/model/entity/Discount.java
@@ -1,17 +1,89 @@
 package com.taxi.model.entity;
 
 
-import java.time.LocalDateTime;
-import java.util.Date;
+import com.taxi.util.MyMath;
+import java.time.LocalDate;
+import java.util.Objects;
 
 public class Discount {
 
+    private int id;
+    private LocalDate effectiveDate;
+    private int discountLevel;
+    private double totalSumCondition;
+    private double discountPercentage;
 
-    //У всіх розрахунках сума округляється до гривень.
+    public Discount() {
+    }
 
+    public Discount(LocalDate effectiveDate, int discountLevel, double totalSumCondition, double discountPercentage) {
+        this.effectiveDate = effectiveDate;
+        this.discountLevel = discountLevel;
+        this.totalSumCondition = totalSumCondition;
+        this.discountPercentage = discountPercentage;
+    }
 
-    private int sumFrom;
+    public int getId() {
+        return id;
+    }
 
-    private int discountPercent;
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public LocalDate getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(LocalDate effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public int getDiscountLevel() {
+        return discountLevel;
+    }
+
+    public void setDiscountLevel(int discountLevel) {
+        this.discountLevel = discountLevel;
+    }
+
+    public double getTotalSumCondition() {
+        return totalSumCondition;
+    }
+
+    public void setTotalSumCondition(double totalSumCondition) {
+        this.totalSumCondition = MyMath.roundToTwoDecimalPlaces(totalSumCondition);
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = MyMath.roundToTwoDecimalPlaces(discountPercentage);
+    }
+
+    @Override
+    public String toString() {
+        return "Discount{" +
+                "id=" + id +
+                ", effectiveDate=" + effectiveDate +
+                ", discountLevel=" + discountLevel +
+                ", totalSumCondition=" + totalSumCondition +
+                ", discountPercentage=" + discountPercentage +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Discount discount = (Discount) o;
+        return id == discount.id && discountLevel == discount.discountLevel && Double.compare(discount.totalSumCondition, totalSumCondition) == 0 && Double.compare(discount.discountPercentage, discountPercentage) == 0 && Objects.equals(effectiveDate, discount.effectiveDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, effectiveDate, discountLevel, totalSumCondition, discountPercentage);
+    }
 }
